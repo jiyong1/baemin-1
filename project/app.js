7f16f69e408415346780c7751cbcff0419970c4a
김한철 <05.hanch.it@gmail.com>
2021-07-09 09:06:13 +0900

    feature : login error msg shake
    로그인 오류시 에러 alert 말고 shake 메세지 추가

--- project/app.js
+++ project/app.js
@@ -6,7 +6,7 @@ const routers = require('./routes')
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()) //post body read to json
 app.use(express.static(path.join(__dirname, 'src')));
